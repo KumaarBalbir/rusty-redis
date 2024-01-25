6fb1ef2564c4e1402d2e315c37e7f8b54d064c85
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-01-25 22:09:59 +0530

    stage2 submission

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{
     net::TcpListener,
     str,
 };
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -12,16 +13,45 @@ fn main() {
     // and it will panic and exit if the binding fails.
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+    //a buffer is created to store incoming data from the network stream.
+    //The buffer is an array of 1024 bytes(u8 elements), initialized to zero.
+    //This buffer will be used to read data sent by clients to the server.
+    //When a client sends a command, the server will read the bytes into this buffer to process the command.
+    //The mut keyword indicates that the contents of buffer can change,
+    //which is necessary because it will be filled with the data read from the network stream.
+    let mut buffer: [u8; 1024] = [0; 1024];
+
     //The for stream in listener.incoming() loop iterates over incoming connections.
     // The incoming method returns an iterator over incoming connections.
     for stream in listener.incoming() {
         // match the stream with the Result type
         match stream {
-            Ok(_stream) => {
+            Ok(mut tcp_stream) => {
                 // When a connection is successfully established
                 // _stream is a placeholder indicating that the
                 // stream is accepted but not explicitly used in this example
                 println!("accepted new connection");
+                // read method for tcp stream, pass buffer as arg to fill it with
+                // incoming data. num_bytes stores the number of bytes read
+                let res = tcp_stream.read(&mut buffer);
+                // match statement to handle the result of read operation
+                match res {
+                    Ok(num_bytes) => {
+                        println!("read {} bytes", num_bytes);
+                        println!("data: {:?}", str::from_utf8(&buffer[..num_bytes]));
+
+                        //check if the received data matches the encoded PING command,
+                        //if it does server responds with the encoded +PONG\r\n message using write method.
+                        if buffer[..num_bytes] == b"*1\r\n$4\r\nPING\r\n"[..num_bytes]
+                            || buffer[..num_bytes] == b"*1\r\n$4\r\nping\r\n"[..num_bytes]
+                        {
+                            let _res_write = tcp_stream.write(b"+PONG\r\n");
+                        }
+                    }
+                    Err(e) => {
+                        println!("error: {}", e);
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
